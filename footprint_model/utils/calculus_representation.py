22bd490b606bdd363c7eb7057ea0c464e3fa9874
Vincent Villet <vincent.villet@gmail.com>
2023-08-18 10:24:13 +0200

    [WIP] model and calculus graph representation

---
+++ footprint_model/utils/calculus_representation.py
@@ -0,0 +1,117 @@
+from pyvis.network import Network
+
+
+def nodes_at_depth(node, depth=0, depth_lists=None, label_len_threshold=-1):
+    if depth_lists is None:
+        depth_lists = {}
+
+    if len(node.label) > label_len_threshold:
+        if depth not in depth_lists:
+            depth_lists[depth] = []
+        for i in range(0, depth):
+            depth_lists[i] = [n for n in depth_lists[i] if n.label != node.label]
+        if node.label not in [n.label for n in depth_lists[depth]]:
+            depth_lists[depth].append(node)
+
+        depth += 1
+
+    if node.left_child:
+        nodes_at_depth(node.left_child, depth, depth_lists, label_len_threshold)
+    if node.right_child:
+        nodes_at_depth(node.right_child, depth, depth_lists, label_len_threshold)
+
+    return depth_lists
+
+
+def calculate_positions(node, label_len_threshold=-1):
+    depth_lists = nodes_at_depth(node, label_len_threshold=label_len_threshold)
+    max_width = max(len(lst) for lst in depth_lists.values())
+    pos = {}
+
+    for depth, nodes in depth_lists.items():
+        num_nodes = len(nodes)
+        for i, n in enumerate(nodes):
+            offset = (num_nodes - 1) / 2
+            x = (i - offset) * (max_width / num_nodes)
+            pos[n.label] = (x, depth + (i % 2)/4)
+
+    return pos
+
+
+def build_graph(root_node, x_multiplier=150, y_multiplier=250, label_len_threshold=-1):
+    G = Network(notebook=True, directed=True, width="1800px", height="900px")
+    G.toggle_physics(False)
+
+    pos = calculate_positions(root_node, label_len_threshold)
+
+    def add_nodes_edges(node, parent_id=None):
+        if len(node.label) > label_len_threshold:
+            G.add_node(
+                node.label, label=node.label, title=str(node.value), x=pos[node.label][0]*x_multiplier,
+                y=pos[node.label][1]*y_multiplier)
+            if parent_id:
+                G.add_edge(parent_id, node.label)
+            current_id = node.label
+        else:
+            current_id = parent_id
+
+        if node.left_child:
+            add_nodes_edges(node.left_child, current_id)
+        if node.right_child:
+            add_nodes_edges(node.right_child, current_id)
+
+    add_nodes_edges(root_node)
+
+    return G
+
+
+if __name__ == "__main__":
+    from footprint_model.constants.sources import SourceValue, Sources
+    from footprint_model.core.user_journey import UserJourney, UserJourneyStep
+    from footprint_model.core.server import Servers
+    from footprint_model.core.storage import Storage
+    from footprint_model.core.service import Service
+    from footprint_model.core.device_population import DevicePopulation, Devices
+    from footprint_model.core.usage_pattern import UsagePattern
+    from footprint_model.core.network import Networks
+    from footprint_model.core.system import System
+    from footprint_model.constants.countries import Countries
+    from footprint_model.constants.units import u
+
+    server = Servers.SERVER
+    storage = Storage(
+        "Default SSD storage",
+        carbon_footprint_fabrication=SourceValue(160 * u.kg, Sources.STORAGE_EMBODIED_CARBON_STUDY),
+        power=SourceValue(1.3 * u.W, Sources.STORAGE_EMBODIED_CARBON_STUDY),
+        lifespan=SourceValue(6 * u.years, Sources.HYPOTHESIS),
+        idle_power=SourceValue(0 * u.W, Sources.HYPOTHESIS),
+        storage_capacity=SourceValue(1 * u.To, Sources.STORAGE_EMBODIED_CARBON_STUDY),
+        power_usage_effectiveness=1.2,
+        country=Countries.GERMANY,
+        data_replication_factor=3,
+        data_storage_duration=10 * u.year
+    )
+    service = Service("Youtube", server, storage, base_ram_consumption=300 * u.Mo,
+                      base_cpu_consumption=2 * u.core)
+
+    streaming_step = UserJourneyStep("20 min streaming", service, 50 * u.ko, (2.5 / 3) * u.Go,
+                                     user_time_spent=20 * u.min, request_duration=4 * u.min)
+    upload_step = UserJourneyStep("0.4s upload", service, 300 * u.ko, 0 * u.Go, user_time_spent=1 * u.s,
+                                  request_duration=0.1 * u.s)
+
+    default_uj = UserJourney("Daily Youtube usage", uj_steps=[streaming_step, upload_step])
+
+    default_device_pop = DevicePopulation(
+        "French laptops", 4e7 * 0.3, Countries.FRANCE, [Devices.LAPTOP])
+
+    default_network = Networks.WIFI_NETWORK
+    usage_pattern = UsagePattern(
+        "Daily Youtube usage", default_uj, default_device_pop,
+        default_network, 365 * u.user_journey / (u.user * u.year), [[7, 23]])
+
+    system = System("system 1", [usage_pattern])
+
+    G = build_graph(system.energy_footprints()["Servers"], label_len_threshold=0)
+    G.show("calculus_output.html")
+    G_raw = build_graph(system.energy_footprints()["Servers"], label_len_threshold=-1)
+    G_raw.show("calculus_output__raw.html")
---
+++ footprint_model/utils/graph_representation.py
@@ -0,0 +1,136 @@
+import os.path
+
+from footprint_model.constants.explainable_quantities import ModelingObject
+from footprint_model.utils.tools import convert_to_list
+
+from pyvis.network import Network
+from selenium import webdriver
+import json
+import time
+
+
+COLOR_MAP = {
+    "Server": "red",
+    "Hardware": "red",
+    "Storage": "brown",
+    "Service": "blue",
+    "UsagePattern": "green",
+    "DevicePopulation": "orange",
+    "UserJourney": "green",
+    "UserJourneyStep": "orange",
+    "TimeIntervals": "green",
+}
+
+USAGE_PATTERN_VIEW_CLASSES_TO_IGNORE = [
+    "System", "UserJourney", "UserJourneyStep", "TimeIntervals", "Network", "Service", "Hardware"]
+SERVICES_VIEW_CLASSES_TO_IGNORE = [
+    "System", "UsagePattern", "TimeIntervals", "Network", "Server", "Storage", "Hardware", "DevicePopulation"]
+SERVICES_AND_INFRA_VIEW_CLASSES_TO_IGNORE = [
+    "UsagePattern", "TimeIntervals", "Network", "Hardware", "DevicePopulation", "System",
+    "UserJourney", "UserJourneyStep"]
+
+WIDTH = 1200
+HEIGHT = 900
+
+
+def build_graph(node, input_graph=None, visited=None, classes_to_ignore=None, width=WIDTH, height=HEIGHT):
+    if classes_to_ignore is None:
+        classes_to_ignore = ["System"]
+    if input_graph is None:
+        input_graph = Network(notebook=True, width=f"{width}px", height=f"{height}px")
+    if visited is None:
+        visited = set()
+
+    if node in visited:
+        return input_graph
+
+    node_type = type(node).__name__
+    if node_type not in classes_to_ignore:
+        input_graph.add_node(id(node), label=f"{node.name}", color=COLOR_MAP.get(node_type, "gray"))
+
+    for attr_name, attr_value in vars(node).items():
+        values = convert_to_list(attr_value)
+        for value in values:
+            if isinstance(value, ModelingObject):
+                value_type = type(value).__name__
+                if value_type not in classes_to_ignore:
+                    input_graph.add_node(id(value), label=f"{value.name}", color=COLOR_MAP.get(value_type, "gray"))
+                    if node_type not in classes_to_ignore:
+                        input_graph.add_edge(id(node), id(value), title=attr_name)
+
+                if value not in visited:
+                    visited.add(node)
+                    build_graph(value, input_graph, visited, classes_to_ignore, width, height)
+
+    return input_graph
+
+
+def capture_screenshot(html_file_path, output_image_path, width, height):
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    driver = webdriver.Chrome(options=options)
+    driver.set_window_size(width, height)
+
+    driver.get(f"file://{html_file_path}")
+    time.sleep(2)
+
+    driver.save_screenshot(output_image_path)
+
+    driver.quit()
+
+
+def save_graph_as_both_html_and_png(input_graph, output_filepath, width=WIDTH, height=HEIGHT):
+    input_graph.set_options(json.dumps({'physics': {"springLength": 250}}))
+    input_graph.show(f"{output_filepath}")
+    capture_screenshot(
+        os.path.abspath(f"{output_filepath}"), f"{output_filepath.replace('html', 'png')}", width, height)
+
+
+if __name__ == "__main__":
+    from footprint_model.constants.sources import SourceValue, Sources
+    from footprint_model.core.user_journey import UserJourney, UserJourneyStep
+    from footprint_model.core.server import Servers
+    from footprint_model.core.storage import Storage
+    from footprint_model.core.service import Service
+    from footprint_model.core.device_population import DevicePopulation, Devices
+    from footprint_model.core.usage_pattern import UsagePattern
+    from footprint_model.core.network import Networks
+    from footprint_model.core.system import System
+    from footprint_model.constants.countries import Countries
+    from footprint_model.constants.units import u
+
+    server = Servers.SERVER
+    storage = Storage(
+        "Default SSD storage",
+        carbon_footprint_fabrication=SourceValue(160 * u.kg, Sources.STORAGE_EMBODIED_CARBON_STUDY),
+        power=SourceValue(1.3 * u.W, Sources.STORAGE_EMBODIED_CARBON_STUDY),
+        lifespan=SourceValue(6 * u.years, Sources.HYPOTHESIS),
+        idle_power=SourceValue(0 * u.W, Sources.HYPOTHESIS),
+        storage_capacity=SourceValue(1 * u.To, Sources.STORAGE_EMBODIED_CARBON_STUDY),
+        power_usage_effectiveness=1.2,
+        country=Countries.GERMANY,
+        data_replication_factor=3,
+        data_storage_duration=10 * u.year
+    )
+    service = Service("Youtube", server, storage, base_ram_consumption=300 * u.Mo,
+                      base_cpu_consumption=2 * u.core)
+
+    streaming_step = UserJourneyStep("20 min streaming on Youtube", service, 50 * u.ko, (2.5 / 3) * u.Go,
+                                     user_time_spent=20 * u.min, request_duration=4 * u.min)
+    upload_step = UserJourneyStep("0.4s of upload", service, 300 * u.ko, 0 * u.Go, user_time_spent=1 * u.s,
+                                  request_duration=0.1 * u.s)
+
+    default_uj = UserJourney("Daily Youtube usage", uj_steps=[streaming_step, upload_step])
+
+    default_device_pop = DevicePopulation(
+        "French Youtube users on laptop", 4e7 * 0.3, Countries.FRANCE, [Devices.LAPTOP])
+
+    default_network = Networks.WIFI_NETWORK
+    usage_pattern = UsagePattern(
+        "Average daily Youtube usage in France on laptop", default_uj, default_device_pop,
+        default_network, 365 * u.user_journey / (u.user * u.year), [[7, 23]])
+
+    system = System("system 1", [usage_pattern])
+
+    G = build_graph(usage_pattern)
+    save_graph_as_both_html_and_png(G, "output.html")
