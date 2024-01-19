10bcf6bc7f45e89101085f103b4e88a36049fd7e
olivier <odelcroi@gmail.com>
2024-01-19 16:47:53 +0100

    wip : refacto of user_journey

--- efootprint/core/usage/user_journey.py
+++ efootprint/core/usage/user_journey.py
@@ -5,58 +5,20 @@ from efootprint.core.service import Service
 from efootprint.core.hardware.servers.server_base_class import Server
 from efootprint.core.hardware.storage import Storage
 from efootprint.logger import logger
-
+from efootprint.core.usage.job import Job
 from typing import List, Set, Type, Optional
 
 
 class UserJourneyStep(ModelingObject):
-    def __init__(self, name: str, service: Optional[Service], data_upload: SourceValue, data_download: SourceValue,
-                 user_time_spent: SourceValue, request_duration: SourceValue = None,
-                 cpu_needed: SourceValue = None, ram_needed: SourceValue = None):
+    def __init__(self, name: str, user_time_spent: SourceValue, jobs: List[Job]):
         super().__init__(name)
-        self.ram_needed = None
-        self.service = service
-        if not data_upload.value.check("[]/[user_journey]"):
-            raise ValueError("Variable 'data_upload' does not have the appropriate '[]/[user_journey]' dimensionality")
-        if not data_download.value.check("[]/[user_journey]"):
-            raise ValueError("Variable 'data_upload' does not have the appropriate '[]/[user_journey]' dimensionality")
-        self.data_upload = data_upload.set_label(f"Data upload of request {self.name}")
-        self.data_download = data_download.set_label(f"Data download of request {self.name}")
 
         if not user_time_spent.value.check("[time]/[user_journey]"):
             raise ValueError(
                 "Variable 'user_time_spent' does not have the appropriate '[time]/[user_journey]' dimensionality")
-        self.user_time_spent = user_time_spent.set_label(f"Time spent on step {self.name}")
-
-        if self.service is None:
-            if (self.data_download.magnitude > 0 or self.data_upload.magnitude > 0 or request_duration
-                    or ram_needed or cpu_needed):
-                raise ValueError(
-                    f"When creating a user journey without service there should be no data transfer and no request"
-                    f" duration or resource need")
-        else:
-            if request_duration is None:
-                request_duration = SourceValue(1 * u.s)
-            if not request_duration.value.check("[time]"):
-                raise ValueError("Variable 'request_duration' does not have the appropriate '[time]' dimensionality")
-            self.request_duration = request_duration.set_label(f"Request duration to {self.service.name} in {self.name}")
-            if request_duration.value > user_time_spent.value * u.uj:
-                # TODO: define a setter method to make this check also when the attribute is updated
-                logger.warning("Variable 'request_duration' is greater than variable 'user_time_spent'")
-
-            if ram_needed is None:
-                ram_needed = SourceValue(100 * u.MB / u.uj)
-            if not ram_needed.value.check("[] / [user_journey]"):
-                raise ValueError(
-                    "Variable 'ram_needed' does not have the appropriate '[] / [user_journey]' dimensionality")
-            self.ram_needed = ram_needed.set_label(f"RAM needed on server {self.service.server.name} to process {self.name}")
-
-            if cpu_needed is None:
-                cpu_needed = SourceValue(1 * u.core / u.uj)
-            if not cpu_needed.value.check("[cpu] / [user_journey]"):
-                raise ValueError(
-                    "Variable 'cpu_needed' does not have the appropriate '[cpu] / [user_journey]' dimensionality")
-            self.cpu_needed = cpu_needed.set_label(f"CPU needed on server {self.service.server.name} to process {self.name}")
+        self.user_time_spent = user_time_spent
+        self.user_time_spent.set_label(f"Time spent on step {self.name}")
+        self.jobs = jobs
 
     @property
     def user_journeys(self):
@@ -66,6 +28,7 @@ class UserJourneyStep(ModelingObject):
     def usage_patterns(self):
         return list(set(sum([uj.usage_patterns for uj in self.user_journeys], start=[])))
 
+    #todo : to be updated
     @property
     def modeling_objects_whose_attributes_depend_directly_on_me(self) -> List:
         if len(self.user_journeys) > 0:
@@ -78,7 +41,8 @@ class UserJourneyStep(ModelingObject):
     def after_init(self):
         self.init_has_passed = True
         self.compute_calculated_attributes()
-
+    
+    #todo : to be updated
     def __mul__(self, other):
         if type(other) not in [int, float]:
             raise ValueError(f"Can only multiply UserJourneyStep with int or float, not {type(other)}")
@@ -101,8 +65,9 @@ class UserJourney(ModelingObject):
     def servers(self) -> Set[Server]:
         servers = set()
         for uj_step in self.uj_steps:
-            if uj_step.service is not None:
-                servers = servers | {uj_step.service.server}
+            for job in uj_step.jobs:
+                if job.service is not None:
+                    servers = servers | {job.service.server}
 
         return servers
 
@@ -110,8 +75,9 @@ class UserJourney(ModelingObject):
     def storages(self) -> Set[Storage]:
         storages = set()
         for uj_step in self.uj_steps:
-            if uj_step.service is not None:
-                storages = storages | {uj_step.service.storage}
+            for job in uj_step.jobs:
+                if job.service is not None: #todo : not necessary condition, service is mandatory in job
+                    storages = storages | {job.service.storage}
 
         return storages
 
@@ -119,8 +85,9 @@ class UserJourney(ModelingObject):
     def services(self) -> List[Service]:
         services = set()
         for uj_step in self.uj_steps:
-            if uj_step.service is not None:
-                services = services | {uj_step.service}
+            for job in uj_step.jobs:
+                if job.service is not None:
+                    services = services | {job.service}
 
         return list(services)
 
@@ -128,6 +95,7 @@ class UserJourney(ModelingObject):
     def usage_patterns(self):
         return self.modeling_obj_containers
 
+    #todo : to be updated
     @property
     def modeling_objects_whose_attributes_depend_directly_on_me(self) -> List[Type["UsagePattern"]]:
         return self.usage_patterns
@@ -148,13 +116,15 @@ class UserJourney(ModelingObject):
     def update_data_download(self):
         all_data_download = 0
         for uj_step in self.uj_steps:
-            all_data_download += uj_step.data_download
+            for job in uj_step.jobs:
+                all_data_download += job.data_download
 
         self.data_download = all_data_download.set_label(f"Data download of {self.name}")
 
     def update_data_upload(self):
         all_data_upload = 0
         for uj_step in self.uj_steps:
-            all_data_upload += uj_step.data_upload
+            for job in uj_step.jobs:
+                all_data_upload += job.data_upload
 
         self.data_upload = all_data_upload.set_label(f"Data upload of {self.name}")
